6956f1905180feb9cbed8fc7b257841833aa5b40
Em <emiliaburdak@gmail.com>
2023-07-12 21:27:51 +0700

    Refactor FlashCard model into separate FlashCard and Deck models

--- flask/Anki/app/body.py
+++ flask/Anki/app/body.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
-from .models import FlashCard, User
+from .models import FlashCard, User, Deck
 from . import db
 
 body = Blueprint('body', __name__)
@@ -10,15 +10,15 @@ body = Blueprint('body', __name__)
 @body.route('/home')
 @login_required
 def home():
-    decks_tuples = FlashCard.query.with_entities(FlashCard.deck).distinct().all()
-    # decks_tuples = [("Talia 1",), ("Talia 2",), ("Talia3,)]
-    decks = [deck_name[0] for deck_name in decks_tuples]
-    # decks = [Talia1, Talia2, Talia3]
+    # deck_names = list(map(lambda x: x.deck_name, user.decks))
+    # all_decks = Deck.query.with_entities(FlashCard.deck).distinct().all()
 
-    # all_flashcards = FlashCard.query.all()
-    # decks = {flashcard.deck for flashcard in all_flashcards}
+    all_decks_tuples = Deck.query.with_entities(Deck.deck_name).all()
+    all_decks_names = [deck_name[0] for deck_name in all_decks_tuples]
 
-    return render_template('home.html', user=current_user, decks=decks)
+    # all_decks_tuples = [("Talia 1",), ("Talia 2",), ("Talia3,)]
+    return render_template('home.html', user=current_user, decks=all_decks_names)
+
 
 @body.route('/add-flashcard', methods=['GET', 'POST'])
 @login_required
@@ -27,13 +27,31 @@ def add_flashcard():
         back_name = request.form.get('back-name')
         front_name = request.form.get('front-name')
         language = request.form.get('language')
-        deck = request.form.get('flashcard-deck')
         sentence = request.form.get('example-sentence')
+        form_deck_name = request.form.get('flashcard-deck')
+        user = current_user
+
+        if not back_name or not front_name or not language or not sentence or not form_deck_name:
+            flash('All fields are required!')
+            return redirect(url_for('new_flashcard.html'))
+
         if len(back_name) < 1 or len(front_name) < 1:
             flash('Word is too short!')
+            return redirect(url_for('new_flashcard.html'))
+
         else:
-            new_flashcard = FlashCard(back_name=back_name, front_name=front_name, language=language, deck=deck,
-                                      sentence=sentence)
+            decks_matching_name = list(filter(lambda x: x.deck_name == form_deck_name, user.decks))
+            if decks_matching_name:
+                deck_id = decks_matching_name[0].id
+
+            else:
+                new_deck = Deck(deck_name=form_deck_name, author_id=user.id, language=language)
+                db.session.add(new_deck)
+                db.session.flush()
+                deck_id = new_deck.id
+
+            new_flashcard = FlashCard(back_name=back_name, front_name=front_name, sentence=sentence,
+                                          author_id=user.id, deck_id=deck_id)
             db.session.add(new_flashcard)
             db.session.commit()
 
@@ -42,10 +60,17 @@ def add_flashcard():
 
 @body.route('/deck/<deck>')
 def deck(deck):
-    flashcards = FlashCard.query.filter_by(deck=deck).all()
+    deck_object = Deck.query.filter_by(deck_name=deck).first()
+    if not deck_object:
+        flash('No such deck')
+        return redirect(url_for('home.html'))
+    deck_id = deck_object.id
+    flashcards = FlashCard.query.filter_by(deck_id=deck_id).all()
     return render_template('deck.html', deck=deck, flashcards=flashcards, user=current_user)
+
 
 @body.route('/display')
 def display():
     all_flashcards = FlashCard.query.all()
     return render_template('check.html', all_flashcards=all_flashcards, user=current_user)
+
--- flask/Anki/app/models.py
+++ flask/Anki/app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
@@ -8,6 +9,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    decks = db.relationship('Deck', backref='user', passive_deletes=True)
+    flashcards = db.relationship('FlashCard', backref='user', passive_deletes=True)
 
 
 class FlashCard(db.Model, UserMixin):
@@ -15,5 +18,13 @@ class FlashCard(db.Model, UserMixin):
     back_name = db.Column(db.String(50))
     front_name = db.Column(db.String(50))
     sentence = db.Column(db.String(500))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id', ondelete='CASCADE'), nullable=False)
+
+
+class Deck(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    deck_name = db.Column(db.String(50))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    flashcards = db.relationship('FlashCard', backref='deck', passive_deletes=True)
     language = db.Column(db.String(50))
-    deck = db.Column(db.String(50))
