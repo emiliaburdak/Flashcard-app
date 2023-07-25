ced4baab579fae8d03d38025594826a392e01c77
Em <emiliaburdak@gmail.com>
2023-07-25 13:38:30 +0700

    wip - refactoring - shortening the function

--- flask/Anki/app/body.py
+++ flask/Anki/app/body.py
@@ -24,10 +24,56 @@ def find_all_decks_names():
     return all_existing_decks_names
 
 
+def find_deck_id_from_existing_deck(select_existing_deck, user):
+    find_selected_deck_object = list(filter(lambda x: x.deck_name == select_existing_deck, user.decks))
+    deck_id = find_selected_deck_object[0].id
+    return deck_id
+
+
+def find_deck_id_from_create_new_deck(create_new_deck, language, user):
+    check_if_matching_names = list(filter(lambda x: x.deck_name == create_new_deck, user.decks))
+    if check_if_matching_names:
+        deck_id = check_if_matching_names[0].id
+        flash('Your new deck name already exist - the flashcard has been assigned to the existing deck')
+        return deck_id
+    else:
+        new_deck = Deck(deck_name=create_new_deck, author_id=user.id, language=language,
+                        last_seen_flashcard_id=None)
+        db.session.add(new_deck)
+        db.session.flush()
+        deck_id = new_deck.id
+        return deck_id
+
+
+def check_if_input_is_valid(back_name, front_name, language, create_new_deck, select_existing_deck):
+    if not back_name or not front_name or not language:
+        return False, 'Please completed all required fields', 'error'
+
+    if len(back_name) < 1 or len(front_name) < 1:
+        return False, 'Word must be at least 1 character long', 'error'
+
+    if create_new_deck and select_existing_deck:
+        return False, 'Please add flashcard to the one of the existing decks or add to new deck', 'error'
+
+    if not create_new_deck and not select_existing_deck:
+        return False, 'Please select deck from existing decks or create new deck', 'error'
+
+    return True, None, None
+
+
+def create_new_flashcard(back_name, front_name, deck_id, sentence):
+    new_flashcard = FlashCard(back_name=back_name, front_name=front_name, sentence=sentence, deck_id=deck_id,
+                              strength=1, last_day_reviewed_at=datetime.datetime.utcnow(),
+                              next_review_at=datetime.datetime.utcnow())
+    db.session.add(new_flashcard)
+    db.session.commit()
+    flash('The flashcard has been created!', category='success')
+    return new_flashcard
+
+
 @body.route('/add-flashcard', methods=['GET', 'POST'])
 @login_required
 def add_flashcard():
-
     all_existing_decks_names = find_all_decks_names()
 
     if request.method == 'POST':
@@ -39,47 +85,19 @@ def add_flashcard():
         create_new_deck = request.form.get('new-deck')
         user = current_user
 
-        if not back_name or not front_name or not language:
-            flash('You have not completed all required fields', category='error')
+        valid_input, message, flash_category = check_if_input_is_valid(back_name, front_name, language, create_new_deck, select_existing_deck)
+        if not valid_input:
+            flash(message, category=flash_category)
             return redirect(url_for('body.add_flashcard'))
 
-        if len(back_name) < 1 or len(front_name) < 1:
-            flash('Word is too short!', category='error')
-            return redirect(url_for('body.add_flashcard'))
-
+        # Depending on chosen field find deck_id to create_new_flashcard
         else:
             if select_existing_deck and not create_new_deck:
-                find_selected_deck_object = list(filter(lambda x: x.deck_name == select_existing_deck, user.decks))
-                deck_id = find_selected_deck_object[0].id
-
-            elif create_new_deck and not select_existing_deck:
-
-                check_if_matching_names = list(filter(lambda x: x.deck_name == create_new_deck, user.decks))
-                if check_if_matching_names:
-                    deck_id = check_if_matching_names[0].id
-                    flash('Your new deck name already exist - the flashcard has been assigned to the existing deck')
-                else:
-                    new_deck = Deck(deck_name=create_new_deck, author_id=user.id, language=language,
-                                    last_seen_flashcard_id=None)
-                    db.session.add(new_deck)
-                    db.session.flush()
-                    deck_id = new_deck.id
-
-            # te dwa warunki daj wyżej dla bardziej czytelnego kodu
-            elif create_new_deck and select_existing_deck:
-                flash('Please add flashcard to the one of the existing decks or add to new deck', category='error')
-                return redirect(url_for('body.add_flashcard'))
-
+                deck_id = find_deck_id_from_existing_deck(select_existing_deck, user)
             else:
-                flash('Select deck from existing decks or create new deck', category='error')
-                return redirect(url_for('body.add_flashcard'))
+                deck_id = find_deck_id_from_create_new_deck(create_new_deck, language, user)
 
-            new_flashcard = FlashCard(back_name=back_name, front_name=front_name, sentence=sentence, deck_id=deck_id,
-                                      strength=1, last_day_reviewed_at=datetime.datetime.utcnow(),
-                                      next_review_at=datetime.datetime.utcnow())
-            db.session.add(new_flashcard)
-            db.session.commit()
-            flash('The flashcard has been created!', category='success')
+        create_new_flashcard(back_name, front_name, deck_id, sentence)
 
     return render_template('new_flashcard.html', user=current_user, existing_decks=all_existing_decks_names)
 
