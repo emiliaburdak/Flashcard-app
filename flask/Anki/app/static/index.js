a7967500d6af4bd322a9da655c7df5a0f67c95b7
Em <emiliaburdak@gmail.com>
2023-07-26 15:22:14 +0700

    hint has been implemented

--- flask/Anki/app/static/index.js
+++ flask/Anki/app/static/index.js
@@ -10,7 +10,18 @@ window.onload = function() {
 };
 
 
+var hintIndex = 0;
+var hintElement = document.getElementById("hint");
+var backName = hintElement.getAttribute('data-back-name');
+
 document.getElementById("answerButton").addEventListener("click", function() {
     document.getElementById("back_name").style.display = "block";
     document.getElementById("sentence").style.display = "block";
+    document.getElementById("hint").style.display = "none";  // hide the hint
+
+});
+
+document.getElementById("hintButton").addEventListener("click", function() {
+    hintElement.style.display = "block";
+    hintElement.textContent = backName.slice(0, ++hintIndex);
 });
